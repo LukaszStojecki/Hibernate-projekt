f672441001634e2a0b4287a94862c602567a6467
lukasz <lukasz.stojecki@o2.pl>
2020-08-23 12:59:18 +0200

    add entity

--- src/main/java/configuration/HibernateConfiguration.java
+++ src/main/java/configuration/HibernateConfiguration.java
@@ -1,14 +1,16 @@
 package configuration;
 
+import Entity.MovieEntity;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 import org.hibernate.cfg.Configuration;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class HibernateConfiguration {
 
-    //private static Logger logger = LoggerFactory.getLogger(HibernateConfiguration.class);
+    private static Logger logger = LoggerFactory.getLogger(HibernateConfiguration.class);
 
     public static void main(String[] args) {
 
@@ -17,6 +19,8 @@ public class HibernateConfiguration {
 
         SessionFactory sessionFactory = configuration.buildSessionFactory();
         Session session = sessionFactory.openSession();
+
+
 
     }
 
